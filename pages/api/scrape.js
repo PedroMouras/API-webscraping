3068403ff9ebea9b9afeb13b39c51780d7fe4a5a
Pedro Moura <pedrosousavn@gmail.com>
2024-03-04 22:38:20 -0300

    implementacao puppeteer encapsulado

--- pages/api/scrape.js
+++ pages/api/scrape.js
@@ -1,42 +1,64 @@
-
-import puppeteer from 'puppeteer';
+import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
+  try {
+    const puppeteerSandboxEndpoint = 'https://try-puppeteer.appspot.com';
 
-  const url = "https://www.mercadolivre.com.br/";
-  const searchFor = "Tab S6 Lite";
+    const response = await fetch(`${puppeteerSandboxEndpoint}/execute`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        code: `
+          const browser = await puppeteer.launch();
+          const page = await browser.newPage();
 
-  await page.goto(url);
-  await page.waitForSelector('#cb1-edit');
-  await page.type('#cb1-edit', searchFor);
-  await Promise.all([
-    page.waitForNavigation(),
-    page.click('.nav-search-btn')
-  ]);
+          const url = "https://www.mercadolivre.com.br/";
+          const searchFor = "Tab S6 Lite";
 
-  const links = await page.$$eval('.ui-search-item__group > a', el => el.map(link => link.href));
+          await page.goto(url);
+          await page.waitForSelector('#cb1-edit');
+          await page.type('#cb1-edit', searchFor);
+          await Promise.all([
+            page.waitForNavigation(),
+            page.click('.nav-search-btn')
+          ]);
 
-  const list = [];
-  let c = 1;
+          const links = await page.$$eval('.ui-search-item__group > a', el => el.map(link => link.href));
 
-  for (const link of links) {
-    if (c === 10) continue;
-    await page.goto(link);
-    await page.waitForSelector('.ui-pdp-title');
-    const title = await page.$eval('.ui-pdp-title', element => element.innerText);
-    const price = await page.$eval('.andes-money-amount__fraction', element => element.innerText);
-    const seller = await page.evaluate(() => {
-      const el = document.querySelector('.ui-pdp-seller__link-trigger');
-      return el ? el.innerText : null;
+          const list = [];
+          let c = 1;
+
+          for (const link of links) {
+            if (c === 10) continue;
+            await page.goto(link);
+            await page.waitForSelector('.ui-pdp-title');
+            const title = await page.$eval('.ui-pdp-title', element => element.innerText);
+            const price = await page.$eval('.andes-money-amount__fraction', element => element.innerText);
+            const seller = await page.evaluate(() => {
+              const el = document.querySelector('.ui-pdp-seller__link-trigger');
+              return el ? el.innerText : null;
+            });
+            const obj = { title, price, seller, link };
+            list.push(obj);
+            c++;
+          }
+
+          await browser.close();
+          list;
+        `,
+      }),
     });
-    const obj = { title, price, seller, link };
-    list.push(obj);
-    c++;
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
+    }
+
+    const data = await response.json();
+    res.status(200).json(data.result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
-
-  await browser.close();
-
-  res.status(200).json(list);
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-// pages/index.js
 
 import { useEffect, useState } from 'react';
 
